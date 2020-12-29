02473fe936ec5c1ef2f1ad836c15b964d96c5cdf
Jukka Aho <ahojukka5@gmail.com>
2020-12-29 16:22:14 +0700

    Exercise 1.6

---
+++ 1.6/README.md
@@ -0,0 +1,7 @@
+# Exercise 1.6
+
+Command to run container:
+
+```bash
+docker run --rm -it docker-clock
+```
---
+++ 1.6/docker-clock.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+APP_NAME="docker-clock"
+IMAGE_NAME="$APP_NAME"
+
+build() {
+    echo "Building image ..."
+    docker build -t $IMAGE_NAME .
+}
+
+run() {
+    echo "Running image ..."
+    docker run --rm -it --name=$APP_NAME $IMAGE_NAME $@
+}
+
+action=$1
+shift 1
+$action "$@"
+
